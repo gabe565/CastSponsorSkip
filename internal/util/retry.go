7f29610540b875515f135c8a20dfeacc6097e4db
Gabe Cook <gabe565@gmail.com>
2023-08-28 13:32:42 -0500

    feat: Allow to halt retry func

--- internal/util/retry.go
+++ internal/util/retry.go
@@ -5,11 +5,32 @@ import (
 	"time"
 )
 
+type ErrHaltRetries struct {
+	Err error
+}
+
+func (err ErrHaltRetries) Error() string {
+	return err.Err.Error()
+}
+
+func (err ErrHaltRetries) Unwrap() error {
+	return err.Err
+}
+
+func HaltRetries(err error) error {
+	return ErrHaltRetries{Err: err}
+}
+
 func Retry(ctx context.Context, attempts uint, sleep time.Duration, fn func(try uint) error) error {
 	var err error
 	for i := uint(0); i < attempts; i += 1 {
 		if err = fn(i); err == nil {
 			return nil
+		} else {
+			switch err := err.(type) {
+			case ErrHaltRetries:
+				return err.Err
+			}
 		}
 
 		select {
