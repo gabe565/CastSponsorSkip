f0891fe1326a12640220811a527660df985eaff1
Gabe Cook <gabe565@gmail.com>
2023-10-15 23:49:58 -0500

    chore(manpages): Validate date flag before recreating manpage dir

--- internal/generate/manpages/main.go
+++ internal/generate/manpages/main.go
@@ -25,6 +25,11 @@ func main() {
 		panic(err)
 	}
 
+	date, err := time.Parse(time.RFC3339, dateParam)
+	if err != nil {
+		panic(err)
+	}
+
 	if err := os.RemoveAll("manpages"); err != nil {
 		panic(err)
 	}
@@ -35,11 +40,6 @@ func main() {
 
 	rootCmd := cmd.NewCommand("beta", "")
 	name := rootCmd.Name()
-
-	date, err := time.Parse(time.RFC3339, dateParam)
-	if err != nil {
-		panic(err)
-	}
 
 	header := doc.GenManHeader{
 		Title:   strings.ToUpper(name),
